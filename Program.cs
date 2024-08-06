356d58fb959d592d32966bb1219cb8f5572fc6cf
IremAdgzl <iremadgzlll@gmail.com>
2024-08-06 13:50:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("İsminiz: ");
+String name = Console.ReadLine();
+Console.WriteLine(name);
+
+
+string str20 = "20";
+int int20 = 20;
+
+string yeni = str20 + int20.ToString();
+Console.WriteLine(yeni);
+
+int int21 = int20 + Convert.ToInt32(str20);
+Console.WriteLine(int21);
+
+int int22 = int20 + int.Parse(str20);
+
+string datetime = DateTime.Now.ToString("dd.MM.yyyy");
+Console.WriteLine(datetime);
+
+string datetime2 = DateTime.Now.ToString("dd/MM/yyyy");
+Console.WriteLine(datetime2);
+
+string hour = DateTime.Now.ToString("HH:mm");
+Console.WriteLine(hour);
