5cb155f41c70407f75907dec2526c0e28c9ea760
IremAdgzl <iremadgzlll@gmail.com>
2024-08-07 12:40:49 +0300

    Add files via upload

---
+++ donguler/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+for (int i = 1; i <= 10; i++)
+{
+    if (i % 2 == 0)
+        continue;
+    Console.WriteLine(i);
+}
