5daba4e61ec8a346c727a6762b4f90aa2b2d660a
IremAdgzl <iremadgzlll@gmail.com>
2024-08-09 16:05:37 +0300

    Add files via upload

---
+++ odevler/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("bir sayı girin: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [] numbers = new int [n];
+
+for(int i = 0; i<n; i++){
+    Console.WriteLine("{0}.sayıyı girin:  ", i+1);
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+
+}
+Console.WriteLine("Çift sayılar : ");
+foreach(int number in numbers){
+    if(number%2==0){
+    Console.WriteLine(number);
+
+    }
+}
